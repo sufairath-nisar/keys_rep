4d601f72b710f84c54c5cc603b1743fb780cdb49
Sufairath C <sufairath.c@gmail.com>
2024-04-18 12:45:36 +0400

    first commit

--- src/Components/Keys.js
+++ src/Components/Keys.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Keys = () => {
+    
     const products=[
-        {name:'LAPTOP',brand : 'dell',qty : 1},
-        {name:'LAPTOP',brand : 'apple',qty : 1},
-        {name:'LAPTOP',brand : 'lenovo',qty : 1},
+        {id:1,name:'LAPTOP',brand : 'dell',qty : 1},
+        {id:2,name:'LAPTOP',brand : 'apple',qty : 1},
+        {id:3,name:'LAPTOP',brand : 'lenovo',qty : 1},
     ]
+    const [productsItem,setItem] = useState(products)
+    const IncrementQty = (id) =>{
+       const newItem = productsItem.map((items)=>(
+        id===items.id ? {...items,qty : ++items.qty}:items
+       ))
+       setItem(newItem)
+    }
+    const DecrementQty = (id) =>{
+        const newItem = productsItem.map((items)=>(
+         id===items.id && items.qty>0 ? {...items,qty : --items.qty}:items
+        ))
+        setItem(newItem)
+     }
+   
   return (
     <div>
         {
-           products.map((item) =>
+           productsItem.map((item) =>
             (
-                <div className='backgrnd-cls'>
+                <div className='backgrnd-cls' key={item.id}>
                 <h1>{item.name}</h1>
                 <p>Brand : {item.brand}</p>
                 <p>Quantity : {item.qty}</p>
+                <button type='button' onClick={() =>IncrementQty(item.id)}>+</button>
+                <button type='button' onClick={() =>DecrementQty(item.id)}>-</button>
                 </div>
             )
             )
